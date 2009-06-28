438c54713a7ca56fba2a7985b6563aa076b17169
David Aguilar <davvid@gmail.com>
2009-06-28 15:09:16 -0700

    git_config: handle configuration entries with no values
    A git-config entry with no value was preventing repo
    from initializing.  This modifies _ReadGit() to handle
    config entries with empty values.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>
    Reported-by: Josh Guilfoyle <jasta00@gmail.com>

---
+++ tests/test_git_config.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+
+import git_config
+
+def fixture(*paths):
+    """Return a path relative to test/fixtures.
+    """
+    return os.path.join(os.path.dirname(__file__), 'fixtures', *paths)
+
+class GitConfigUnitTest(unittest.TestCase):
+    """Tests the GitConfig class.
+    """
+    def setUp(self):
+        """Create a GitConfig object using the test.gitconfig fixture.
+        """
+        config_fixture = fixture('test.gitconfig')
+        self.config = git_config.GitConfig(config_fixture)
+
+    def test_GetString_with_empty_config_values(self):
+        """
+        Test config entries with no value.
+
+        [section]
+            empty
+
+        """
+        val = self.config.GetString('section.empty')
+        self.assertEqual(val, None)
+
+    def test_GetString_with_true_value(self):
+        """
+        Test config entries with a string value.
+
+        [section]
+            nonempty = true
+
+        """
+        val = self.config.GetString('section.nonempty')
+        self.assertEqual(val, 'true')
+
+if __name__ == '__main__':
+    unittest.main()
