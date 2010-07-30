b715b148076ef589e806a98a183c8f4468f75cf6
Patrick Dubroy <dubroy@google.com>
2010-07-29 17:10:47 -0700

    Fix for handling values of EDITOR which contain a space.
    The shell swallows the 0th arg, which was the filename. Simple fix
    is to pass in an extra arg for the shell to swallow.
    
    Change-Id: Iad6304ba9ccea6e7262ee06ef87d3dac57dbde81

--- editor.py
+++ editor.py
@@ -82,7 +82,7 @@ least one of these before using this command."""
       fd = None
 
       if re.compile("^.*[$ \t'].*$").match(editor):
-        args = [editor + ' "$@"']
+        args = [editor + ' "$@"', 'sh']
         shell = True
       else:
         args = [editor]
