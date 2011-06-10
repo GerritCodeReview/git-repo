06bfbee75ff1de6b7e3c8d53581eb784be939534
Brian Harring <brian.harring@intel.com>
2011-06-09 23:33:36 -0700

    add manifest intersection support
    available via `repo manifest --intersect <some-manifest.xml>`,
    default mode is to just take the tree's manifest, filtering
    everything that doesn't intersect the supplied manifest.  If
    -r is specified, HEAD, while unmodified, is treated as the merge-base
    between the intersecting manifest and the tree's manifest.
    
    Signed-off-by: Brian Harring <brian.harring@intel.com>
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>
    Change-Id: I29f7b2f8dc2bd337116de0e7c4f21955490ce784

--- subcmds/manifest.py
+++ subcmds/manifest.py
@@ -17,12 +17,13 @@ import os
 import sys
 
 from command import PagedCommand
+from manifest_xml import XmlManifest
 
 class Manifest(PagedCommand):
   common = False
   helpSummary = "Manifest inspection utility"
   helpUsage = """
-%prog [-o {-|NAME.xml} [-r]]
+%prog [-o {-|NAME.xml} [-r] [--intersect MANIFEST.xml [--derive-from-target]]]
 """
   _helpDescription = """
 
@@ -52,24 +53,80 @@ in a Git repository for use during future 'repo init' invocations.
                  dest='output_file',
                  help='File to save the manifest to',
                  metavar='-|NAME.xml')
+    p.add_option('-i', '--intersect-manifest', dest='intersect_manifest',
+                 action='store',
+                 help='Manifest to perform an intersection '
+                 'against.  If -r is specified, intersect behaviour '
+                 'is behaved to generate the merge-base as HEAD.')
+    p.add_option('-d', '--derive-from-target',
+                 default=False, action='store_true',
+                 help="normally --intersect-manifest just derives from the "
+                 "checked out manifest, filtering that manifest.  This option "
+                 "inverts the behaviour; essentially mapping the target into "
+                 "into the checked out manifest.  Primarily of use when "
+                 "trying to generate intersections for use with the release "
+                 "subcommand.  Essentially, think of it this way- without this, "
+                 "it'll modify the checked out manifest into an intersection.  "
+                 "With this option on, it modifies the target manifest into an "
+                 "intersection")
 
-  def _Output(self, opt):
+  def _Output(self, opt, manifest):
     if opt.output_file == '-':
       fd = sys.stdout
     else:
       fd = open(opt.output_file, 'w')
-    self.manifest.Save(fd,
+    manifest.Save(fd,
                        peg_rev = opt.peg_rev)
     fd.close()
     if opt.output_file != '-':
       print >>sys.stderr, 'Saved manifest to %s' % opt.output_file
 
+  def intersect_manifest(self, source, target, source_is_authorative=True, intersect_rev=False):
+    target_paths = dict((p.relpath, p) for p in target.projects.itervalues())
+    for source_name, source_p in source.projects.items():
+      intersecting_p = target_paths.get(source_p.relpath)
+      if intersecting_p is None:
+        del source.projects[source_name]
+        continue
+      elif intersect_rev:
+        if source_is_authorative:
+          remote_name = source_p.remote.name
+        else:
+          remote_name = intersecting_p.remote.name
+        ref1 = 'remotes/%s/%s' % (remote_name, source_p.revisionExpr)
+        ref2 = 'remotes/%s/%s' % (remote_name, intersecting_p.revisionExpr)
+        rev = source_p.work_git.merge_base(ref1, ref2)
+        source_p.revisionExpr = rev
+    return source
+
   def Execute(self, opt, args):
     if args:
       self.Usage()
 
+    manifest = self.manifest
+    if opt.intersect_manifest is not None:
+      masking_manifest = XmlManifest(manifest.repodir)
+      # note we're exploiting an underlying os.path.join behaviour;
+      # join("/dar/foon", "/absolute") == "/absolute"
+      masking_manifest.Override(os.path.abspath(opt.intersect_manifest))
+      if opt.derive_from_target:
+        args = [masking_manifest, manifest]
+      else:
+        args = [manifest, masking_manifest]
+      manifest = self.intersect_manifest(intersect_rev=opt.peg_rev,
+        source_is_authorative=not opt.derive_from_target, *args)
+      # note that intersect_manifest will rewrite the revision targets
+      # as such, peg_rev is no longer needed (it does the exact wrong thing actually)
+      # we honor it's intent, while suppressing the save's behaviour since we
+      # don't need it converting HEAD to a sha1 (we've already forced the sha1)
+      opt.peg_rev = False
+    elif opt.derive_from_target:
+      print >>sys.stderr, ("--derive-from-target shouldn't be used without "
+        "--intersect-manifest")
+      sys.exit(1)
+
     if opt.output_file is not None:
-      self._Output(opt)
+      self._Output(opt, manifest)
       return
 
     print >>sys.stderr, 'error: no operation to perform'
