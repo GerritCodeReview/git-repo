9e101f753cd911c186c088786c8cb36b41746b1e
Mickaël Salaün <mic@digikod.net>
2012-08-05 13:12:32 +0200

    Be able to use Repo without its repository
    Don't try updating Repo if there is no repository and run the command
    anyway.
    
    Allow Repo to be used without the Git ".repo/repo" for each different
    repo workdir.
    This can be usefull to centralize Repo into a main directory (e.g.
    ~/.repo/repo or /usr/share/git-repo).
    
    Change-Id: I2f7e49cc3e533c7f94d425a2a45535bd4e12d4cd

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -145,7 +145,10 @@ later is required to fix a server side protocol bug.
 """
 
   def _Options(self, p, show_smart=True):
-    self.jobs = self.manifest.default.sync_j
+    if self.manifest.repoProject.Exists:
+        default_jobs = " (default %d)" % self.manifest.default.sync_j
+    else:
+        default_jobs = ""
 
     p.add_option('-f', '--force-broken',
                  dest='force_broken', action='store_true',
@@ -167,7 +170,7 @@ later is required to fix a server side protocol bug.
                  help='be more quiet')
     p.add_option('-j', '--jobs',
                  dest='jobs', action='store', type='int',
-                 help="projects to fetch simultaneously (default %d)" % self.jobs)
+                 help="projects to fetch simultaneously%s" % default_jobs)
     p.add_option('-m', '--manifest-name',
                  dest='manifest_name',
                  help='temporary manifest to use for this sync', metavar='NAME.xml')
@@ -565,7 +568,7 @@ later is required to fix a server side protocol bug.
     if not opt.local_only:
       to_fetch = []
       now = time.time()
-      if _ONE_DAY_S <= (now - rp.LastFetch):
+      if rp.Exists and _ONE_DAY_S <= (now - rp.LastFetch):
         to_fetch.append(rp)
       to_fetch.extend(all_projects)
       to_fetch.sort(key=self._fetch_times.Get, reverse=True)
@@ -609,6 +612,8 @@ def _PostRepoUpgrade(manifest, quiet=False):
       project.PostRepoUpgrade()
 
 def _PostRepoFetch(rp, no_repo_verify=False, verbose=False):
+  if not rp.Exists:
+    return
   if rp.HasChanges:
     print('info: A new version of repo is available', file=sys.stderr)
     print(file=sys.stderr)
