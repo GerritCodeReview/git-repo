c68fd84c5a1cb11d0863a8555c4e996f1a5c49e4
Jooncheol Park <jooncheol@gmail.com>
2012-08-27 02:25:59 +0900

    Fix ManifestParseError when first child node is comment
    If the first line of manifest.xml is a XML comment, root.childNodes[0]
    is not a 'manifest' element node. The python minidom module will makes
    a 'Comment' node as root.childNodes[0]. Since the original code only
    checks whether the first child node is 'manifest', it couldn't do any
    command including 'sync' due to the 'ManifestParseError' exception. This
    patch could allow the comments between '<?xml ...?>' and '<manifest>' in
    the manifest.xml file.
    
    Change-Id: I0b81dea4f806965eca90f704c8aa7df49c579402

--- manifest_xml.py
+++ manifest_xml.py
@@ -309,9 +309,13 @@ class XmlManifest(object):
     if not root or not root.childNodes:
       raise ManifestParseError("no root node in %s" % (path,))
 
-    config = root.childNodes[0]
-    if config.nodeName != 'manifest':
-      raise ManifestParseError("no <manifest> in %s" % (path,))
+    config = None
+    for n in root.childNodes:
+        if n.nodeName == 'manifest':
+            config = n
+            break
+    else:
+        raise ManifestParseError("no <manifest> in %s" % (path,))
 
     nodes = []
     for node in config.childNodes:
