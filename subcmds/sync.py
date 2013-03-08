3eed519af70d4a804b85953b0f5b89ae9b27b390
Tim Kilbourn <tkilbourn@google.com>
2013-03-08 15:02:49 -0800

    Reload the correct manifest during sync.
    Fix for issue #134
    https://code.google.com/p/git-repo/issues/detail?id=134
    
    Change-Id: I94c2dea5dd63917e3f9c90cbd628921d7d61b12a

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -372,6 +372,13 @@ later is required to fix a server side protocol bug.
       print('\nerror: Exited sync due to gc errors', file=sys.stderr)
       sys.exit(1)
 
+  def _ReloadManifest(self):
+    if self.override_manifest_name:
+      # Override calls _Unload already
+      self.manifest.Override(self.override_manifest_name)
+    else:
+      self.manifest._Unload()
+
   def UpdateProjectList(self):
     new_project_paths = []
     for project in self.GetProjects(None, missing_ok=True):
@@ -462,7 +469,10 @@ later is required to fix a server side protocol bug.
         sys.exit(1)
 
     if opt.manifest_name:
-      self.manifest.Override(opt.manifest_name)
+      self.override_manifest_name = opt.manifest_name
+      self.manifest.Override(self.override_manifest_name)
+    else:
+      self.override_manifest_name = None
 
     if opt.smart_sync or opt.smart_tag:
       if not self.manifest.manifest_server:
@@ -571,7 +581,7 @@ later is required to fix a server side protocol bug.
       mp.Sync_LocalHalf(syncbuf)
       if not syncbuf.Finish():
         sys.exit(1)
-      self.manifest._Unload()
+      self._ReloadManifest()
       if opt.jobs is None:
         self.jobs = self.manifest.default.sync_j
     all_projects = self.GetProjects(args,
@@ -596,7 +606,7 @@ later is required to fix a server side protocol bug.
       # Iteratively fetch missing and/or nested unregistered submodules
       previously_missing_set = set()
       while True:
-        self.manifest._Unload()
+        self._ReloadManifest()
         all_projects = self.GetProjects(args,
                                         missing_ok=True,
                                         submodules_ok=opt.fetch_submodules)
