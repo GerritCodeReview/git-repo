63d63d9478ce853ea024642b684db239c331dfc5
Julien Campergue <julien.campergue@parrot.com>
2014-01-13 12:00:31 +0100

    Keep comments in manifest if they are children of project.
    We sometimes need to keep some comments in generated manifests,
    mainly for explanatory reasons.
    
    Change-Id: I2a864ae0fc6a96b201b079c8d64e2bd5f56703a0
    Signed-off-by: Julien Campergue <julien.campergue@parrot.com>

--- project.py
+++ project.py
@@ -556,6 +556,7 @@ class Project(object):
     self.snapshots = {}
     self.copyfiles = []
     self.annotations = []
+    self.comments = []
     self.config = GitConfig.ForRepository(
                     gitdir = self.gitdir,
                     defaults =  self.manifest.globalConfig)
@@ -1270,6 +1271,9 @@ class Project(object):
         return
     else:
       syncbuf.later1(self, _doff)
+
+  def AddComment(self, comment):
+    self.comments.append(comment)
 
   def AddCopyFile(self, src, dest, absdest):
     # dest should already be an absolute path, but src is project relative
