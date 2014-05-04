e1b44910b08bb6cb0e59910d6f0143edd40952fd
Anthony King <anthonydking@gmail.com>
2014-05-04 15:53:04 +0100

    Add support for Python3
    execfile is not in python 3. Add an if statement to switch between exec and execfile.
    
    iter.next() is not in python. change to next(iter). This is backwards compatible to Python 2.6.
    
    dict.values(), .items(), and .keys() produce dict_* objects rather than list objects in Python 3.
    Wrap these to convert them to list objects.
    
    map() produces a map object in Python3. Wrap these to convert them to list objects.
    
    unicode has become str in Python 3. If using python 3, define unicode as str.
    
    PS2:
    enforce Protocol 2 on Pickle. Protocol 3 is not supported in Python 2.x.
    This allows the user to switch between Python 2 and 3 without issues.
    
    PS3:
    Only warn about using Python 3 when running the repo script directly
    This fixes the issue where the user is warned twice
    
    Bug: Issue 117
    Change-Id: I2d8a66954bd2ba1f8b02916697b74dec6093a726

--- git_config.py
+++ git_config.py
@@ -227,7 +227,7 @@ class GitConfig(object):
     d = self._section_dict
     if d is None:
       d = {}
-      for name in self._cache.keys():
+      for name in list(self._cache.keys()):
         p = name.split('.')
         if 2 == len(p):
           section = p[0]
@@ -283,7 +283,7 @@ class GitConfig(object):
     try:
       fd = open(self._pickle, 'wb')
       try:
-        pickle.dump(cache, fd, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(cache, fd, protocol=2)
       finally:
         fd.close()
     except IOError:
--- project.py
+++ project.py
@@ -438,7 +438,11 @@ class RepoHook(object):
       # and  convert to a HookError w/ just the failing traceback.
       context = {}
       try:
-        execfile(self._script_fullpath, context)
+        if is_python3():
+          exec(compile(open(self._script_fullpath).read(),
+                       self._script_fullpath, 'exec'), context)
+        else:
+          execfile(self._script_fullpath, context)
       except Exception:
         raise HookError('%s\nFailed to import %s hook; see traceback above.' % (
                         traceback.format_exc(), self._hook_type))
@@ -686,7 +690,7 @@ class Project(object):
     all_refs = self._allrefs
     heads = {}
 
-    for name, ref_id in all_refs.items():
+    for name, ref_id in list(all_refs.items()):
       if name.startswith(R_HEADS):
         name = name[len(R_HEADS):]
         b = self.GetBranch(name)
@@ -695,7 +699,7 @@ class Project(object):
         b.revision = ref_id
         heads[name] = b
 
-    for name, ref_id in all_refs.items():
+    for name, ref_id in list(all_refs.items()):
       if name.startswith(R_PUB):
         name = name[len(R_PUB):]
         b = heads.get(name)
@@ -797,8 +801,8 @@ class Project(object):
       out.nl()
 
     paths = list()
-    paths.extend(di.keys())
-    paths.extend(df.keys())
+    paths.extend(list(di.keys()))
+    paths.extend(list(df.keys()))
     paths.extend(do)
 
     for p in sorted(set(paths)):
@@ -893,13 +897,13 @@ class Project(object):
       all_refs = self._allrefs
     heads = set()
     canrm = {}
-    for name, ref_id in all_refs.items():
+    for name, ref_id in list(all_refs.items()):
       if name.startswith(R_HEADS):
         heads.add(name)
       elif name.startswith(R_PUB):
         canrm[name] = ref_id
 
-    for name, ref_id in canrm.items():
+    for name, ref_id in list(canrm.items()):
       n = name[len(R_PUB):]
       if R_HEADS + n not in heads:
         self.bare_git.DeleteRef(name, ref_id)
@@ -910,14 +914,14 @@ class Project(object):
     heads = {}
     pubed = {}
 
-    for name, ref_id in self._allrefs.items():
+    for name, ref_id in list(self._allrefs.items()):
       if name.startswith(R_HEADS):
         heads[name[len(R_HEADS):]] = ref_id
       elif name.startswith(R_PUB):
         pubed[name[len(R_PUB):]] = ref_id
 
     ready = []
-    for branch, ref_id in heads.items():
+    for branch, ref_id in list(heads.items()):
       if branch in pubed and pubed[branch] == ref_id:
         continue
       if selected_branch and branch != selected_branch:
@@ -1756,7 +1760,7 @@ class Project(object):
         ids = set(all_refs.values())
         tmp = set()
 
-        for r, ref_id in GitRefs(ref_dir).all.items():
+        for r, ref_id in list(GitRefs(ref_dir).all.items()):
           if r not in all_refs:
             if r.startswith(R_TAGS) or remote.WritesTo(r):
               all_refs[r] = ref_id
@@ -2321,8 +2325,8 @@ class Project(object):
           out = iter(out[:-1].split('\0'))  # pylint: disable=W1401
           while out:
             try:
-              info = out.next()
-              path = out.next()
+              info = next(out)
+              path = next(out)
             except StopIteration:
               break
 
@@ -2348,7 +2352,7 @@ class Project(object):
             info = _Info(path, *info)
             if info.status in ('R', 'C'):
               info.src_path = info.path
-              info.path = out.next()
+              info.path = next(out)
             r[info.path] = info
         return r
       finally:
@@ -2465,7 +2469,7 @@ class Project(object):
           if not git_require((1, 7, 2)):
             raise ValueError('cannot set config on command line for %s()'
                              % name)
-          for k, v in config.items():
+          for k, v in list(config.items()):
             cmdv.append('-c')
             cmdv.append('%s=%s' % (k, v))
         cmdv.append(name)
