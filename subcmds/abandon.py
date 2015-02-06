d3b303bd0d3069cc85661603bc686caede2c3990
科 李 <outofkurt@gmail.com>
2015-02-06 08:44:32 +0000

    add parameter '--all' and '--verbose' to repo abandon cmd
    repo abandon --all
    will abandon all branch detected
    
    repo abandon --verbose
    will give info for each branch; for example:
    
    $ repo abandon --all --verbose
    Abandon local_branch    [Y/n]? y
    Abandoned in 1 project(s):
      build
    Abandon feature_branch  [Y/n]? n
    
    Change-Id: I2ca64dc93cd4890e24c1a6e7e7086b5c10f39166

--- subcmds/abandon.py
+++ subcmds/abandon.py
@@ -23,7 +23,7 @@ class Abandon(Command):
   common = True
   helpSummary = "Permanently abandon a development branch"
   helpUsage = """
-%prog <branchname> [<project>...]
+%prog [<branchname> [<project>...] | --all] [--verbose]
 
 This subcommand permanently abandons a development branch by
 deleting it (and all its history) from your local repository.
@@ -31,41 +31,74 @@ deleting it (and all its history) from your local repository.
 It is equivalent to "git branch -D <branchname>".
 """
 
+  def _Options(self, p):
+    p.add_option('--all', default=False,
+                 action='store_true', dest='abandon_all',
+                 help='Abandon all branch detected.')
+    p.add_option('--verbose', default=False,
+                 action='store_true', dest='abandon_deputed',
+                 help='Verbose mode. Give info for each branch.')
+
+  def _get_verbose_mode_result(self, nb):
+    '''_get_verbose_mode_result
+    '''
+    while True:
+      print('Abandon %s\t[Y/n]? ' % nb, end='')
+      result = raw_input()
+      if result.lower() in ['', 'y']:
+        return True
+      if 'n' == result.lower():
+        return False
+
   def Execute(self, opt, args):
-    if not args:
+    if not args and not opt.abandon_all:
       self.Usage()
 
-    nb = args[0]
-    if not git.check_ref_format('heads/%s' % nb):
-      print("error: '%s' is not a valid name" % nb, file=sys.stderr)
-      sys.exit(1)
-
-    nb = args[0]
-    err = []
-    success = []
-    all_projects = self.GetProjects(args[1:])
-
-    pm = Progress('Abandon %s' % nb, len(all_projects))
-    for project in all_projects:
-      pm.update()
-
-      status = project.AbandonBranch(nb)
-      if status is not None:
-        if status:
-          success.append(project)
-        else:
-          err.append(project)
-    pm.end()
-
-    if err:
-      for p in err:
-        print("error: %s/: cannot abandon %s" % (p.relpath, nb),
-              file=sys.stderr)
-      sys.exit(1)
-    elif not success:
-      print('error: no project has branch %s' % nb, file=sys.stderr)
-      sys.exit(1)
+    nbs = {}
+    if opt.abandon_all:
+      projects = self.GetProjects([])
+      for project in projects:
+        for name, b in project.GetBranches().items():
+          if not name in nbs:
+            nbs[name] = []
+          nbs[name].append(project)
     else:
-      print('Abandoned in %d project(s):\n  %s'
-            % (len(success), '\n  '.join(p.relpath for p in success)),
-            file=sys.stderr)
+      nb = args[0]
+      if not git.check_ref_format('heads/%s' % nb):
+        print("error: '%s' is not a valid name" % nb, file=sys.stderr)
+        sys.exit(1)
+      all_projects = self.GetProjects(args[1:])
+      nbs[nb] = all_projects
+
+    for nb in nbs:
+      if opt.abandon_deputed:
+        if not self._get_verbose_mode_result(nb):
+          continue
+      err = []
+      success = []
+      all_projects = nbs[nb]
+
+      pm = Progress('Abandon %s' % nb, len(all_projects))
+      for project in all_projects:
+        pm.update()
+
+        status = project.AbandonBranch(nb)
+        if status is not None:
+          if status:
+            success.append(project)
+          else:
+            err.append(project)
+      pm.end()
+
+      if err:
+        for p in err:
+          print("error: %s/: cannot abandon %s" % (p.relpath, nb),
+                file=sys.stderr)
+        sys.exit(1)
+      elif not success:
+        print('error: no project has branch %s' % nb, file=sys.stderr)
+        sys.exit(1)
+      else:
+        print('Abandoned in %d project(s):\n  %s'
+              % (len(success), '\n  '.join(p.relpath for p in success)),
+              file=sys.stderr)
