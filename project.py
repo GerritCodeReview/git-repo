c183efdbd42b20c14e161d87f3e94802bf5832a6
Aymen Bouaziz <aymen.bouaziz@parrot.com>
2016-06-24 14:34:08 +0200

    Fix submodule checkout error when using sync-s option
    When sync-s="true" option is used, the checkout of a submodule will try
    to use the revision attribute of the parent project.
    
    If this revision is a tag, the checkout will fail if the submodule is
    not tagged with this same tag.
    
    The proposed solution is to use the git commit id as revisionExpr for
    submodules.
    
    Change-Id: Ie8390a11957fd6a9c61289c6861d13cb3fa11678

--- project.py
+++ project.py
@@ -1754,7 +1754,7 @@ class Project(object):
                            objdir=objdir,
                            worktree=worktree,
                            relpath=relpath,
-                           revisionExpr=self.revisionExpr,
+                           revisionExpr=rev,
                            revisionId=rev,
                            rebase=self.rebase,
                            groups=self.groups,
