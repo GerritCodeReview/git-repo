fef9f21b28d3b59804fdd5db2bbb86e797051dab
Renaud Paquay <rpaquay@google.com>
2016-11-01 18:28:01 -0700

    Fix misplaced file separator string.replace call
    Project names are stored as path using the '/' file separator, and
    stored in a dictionary as keys.
    
    Change-Id: Ide40dfe840958ac0d46caae5f77f1a49d71c9d90

--- project.py
+++ project.py
@@ -687,7 +687,7 @@ class Project(object):
     self.gitdir = gitdir.replace('\\', '/')
     self.objdir = objdir.replace('\\', '/')
     if worktree:
-      self.worktree = os.path.normpath(worktree.replace('\\', '/'))
+      self.worktree = os.path.normpath(worktree).replace('\\', '/')
     else:
       self.worktree = None
     self.relpath = relpath
