a2801187d6a1df13f477bef022ed6dedd7a1e2a0
Renaud Paquay <rpaquay@google.com>
2017-01-27 11:41:12 -0800

    Provide more specific error message for symlinks errors on Windows
    Change-Id: Ia6099beef37ae6b6143eba243fe7fbe02b74a9bb

--- project.py
+++ project.py
@@ -2351,7 +2351,7 @@ class Project(object):
             os.path.relpath(stock_hook, os.path.dirname(dst)), dst)
       except OSError as e:
         if e.errno == errno.EPERM:
-          raise GitError('filesystem must support symlinks')
+          raise GitError(self._get_symlink_error_message())
         else:
           raise
 
@@ -2465,7 +2465,7 @@ class Project(object):
 
       except OSError as e:
         if e.errno == errno.EPERM:
-          raise DownloadError('filesystem must support symlinks')
+          raise DownloadError(self._get_symlink_error_message())
         else:
           raise
 
@@ -2503,6 +2503,14 @@ class Project(object):
       if init_dotgit:
         platform_utils.rmtree(dotgit)
       raise
+
+  def _get_symlink_error_message(self):
+    if platform_utils.isWindows():
+      return ('Unable to create symbolic link. Please re-run the command as '
+              'Administrator, or see '
+              'https://github.com/git-for-windows/git/wiki/Symbolic-Links '
+              'for other options.')
+    return 'filesystem must support symlinks'
 
   def _gitdir_path(self, path):
     return platform_utils.realpath(os.path.join(self.gitdir, path))
