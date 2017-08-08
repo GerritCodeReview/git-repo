73070449c382d9a9720b7a1133d990f3b8b34030
Łukasz Gardoń <garjack555@gmail.com>
2017-08-08 10:18:11 +0200

    Disabling globally verification ssl module during uploading review to gerrit.
    This patch allow to bypass verification ssl certification
    based on provided with change in PEP-476, more details here:
    https://www.python.org/dev/peps/pep-0476/#opting-out
    
    Change-Id: If2e15f5a273c18a700eb5093ca8a4d5a4cbf80cd

--- subcmds/upload.py
+++ subcmds/upload.py
@@ -17,6 +17,16 @@ from __future__ import print_function
 import copy
 import re
 import sys
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 from command import InteractiveCommand
 from editor import Editor
