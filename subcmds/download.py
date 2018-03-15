9753b0aba59102e476af4c80739eeafd4b283dbb
Akshay Verma <akshayverma948@gmail.com>
2018-03-15 21:56:30 +0530

    Download latest patch when no patch is specified
    When someone does "repo download -c <project> <change>"
    without specifying a patch number, by default patch 1 is
    downloaded. An alternative is to look for the latest patch
    and download the same when no explicit patch is given.
    This commit does the same by identifying the latest patch
    using "git ls-remote".
    
    Change-Id: Ia5fa7364415f53a3d9436df4643e38f3c90ded58

--- subcmds/download.py
+++ subcmds/download.py
@@ -62,6 +62,12 @@ If no project is specified try to use current directory as a project.
           ps_id = int(m.group(2))
         else:
           ps_id = 1
+          regex = 'refs/changes/%2.2d/%d/(\d+)' % (chg_id % 100, chg_id)
+          output = project._LsRemote()
+          for line in output.splitlines():
+            match = re.search(regex, line, re.I)
+            if match:
+              ps_id = int(match.group(1)) if int(match.group(1)) > ps_id else ps_id
         to_get.append((project, chg_id, ps_id))
       else:
         project = self.GetProjects([a])[0]
