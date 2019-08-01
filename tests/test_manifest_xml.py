de33974f0740b266efa4cec423da1ca7437e9144
Mike Frysinger <vapier@google.com>
2019-07-31 23:32:58 -0400

    manifest: add basic path checks for <copyfile> & <linkfile>
    Reject paths in <copyfile> & <linkfile> that point outside of their
    respective scopes.  This validates paths while parsing the manifest
    as this should be quick & cheap: we don't access the filesystem as
    this code runs before we've synced.
    
    Bug: https://crbug.com/gerrit/11218
    Change-Id: I8e17bb91f3f5b905a9d76391b29fbab4cb77aa58

---
+++ tests/test_manifest_xml.py
@@ -0,0 +1,83 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unittests for the manifest_xml.py module."""
+
+from __future__ import print_function
+
+import unittest
+
+import error
+import manifest_xml
+
+
+class ManifestValidateFilePaths(unittest.TestCase):
+  """Check _ValidateFilePaths helper.
+
+  This doesn't access a real filesystem.
+  """
+
+  def checkBoth(self, *args):
+    manifest_xml.XmlManifest._ValidateFilePaths('copyfile', *args)
+    manifest_xml.XmlManifest._ValidateFilePaths('linkfile', *args)
+
+  def testNormalPath(self):
+    """Make sure good paths are accepted."""
+    self.checkBoth('foo', 'bar')
+    self.checkBoth('foo/bar', 'bar')
+    self.checkBoth('foo', 'bar/bar')
+    self.checkBoth('foo/bar', 'bar/bar')
+
+  def testSymlinkTargets(self):
+    """Some extra checks for symlinks."""
+    def check(*args):
+      manifest_xml.XmlManifest._ValidateFilePaths('linkfile', *args)
+
+    # We allow symlinks to end in a slash since we allow them to point to dirs
+    # in general.  Technically the slash isn't necessary.
+    check('foo/', 'bar')
+
+  def testBadPaths(self):
+    """Make sure bad paths (src & dest) are rejected."""
+    PATHS = (
+        '..',
+        '../',
+        './',
+        'foo/',
+        './foo',
+        '../foo',
+        'foo/./bar',
+        'foo/../../bar',
+        '/foo',
+        './../foo',
+        '.git/foo',
+        # Check case folding.
+        '.GIT/foo',
+        'blah/.git/foo',
+        '.repo/foo',
+        '.repoconfig',
+        # Block ~ due to 8.3 filenames on Windows filesystems.
+        '~',
+        'foo~',
+        'blah/foo~',
+        # Block Unicode characters that get normalized out by filesystems.
+        u'foo\u200Cbar',
+    )
+    for path in PATHS:
+      self.assertRaises(
+          error.ManifestInvalidPathError, self.checkBoth, path, 'a')
+      self.assertRaises(
+          error.ManifestInvalidPathError, self.checkBoth, 'a', path)
