74a3a70cc7b151d89eea5d637f3105ca69f3a37d
Mike Frysinger <vapier@google.com>
2019-11-15 01:19:03 -0500

    upload/editor: fix bytes/string confusion
    The upload module tries to turn the strings into bytes before passing
    to EditString, but it combines bytes & strings causing an error.  The
    return value might be bytes or string, but the caller only expects a
    string.  Lets simplify this by sticking to strings everywhere and have
    EditString take care of converting to/from bytes when reading/writing
    the underlying files.  This also avoids possible locale confusion when
    reading the file by forcing UTF-8 everywhere.
    
    Bug: https://crbug.com/gerrit/11929
    Change-Id: I07b146170c5e8b5b0500a2c79e4213cd12140a96

--- editor.py
+++ editor.py
@@ -68,11 +68,14 @@ least one of these before using this command.""", file=sys.stderr)
   def EditString(cls, data):
     """Opens an editor to edit the given content.
 
-       Args:
-         data        : the text to edit
+    Args:
+      data: The text to edit.
 
-      Returns:
-        new value of edited text; None if editing did not succeed
+    Returns:
+      New value of edited text.
+
+    Raises:
+      EditorError: The editor failed to run.
     """
     editor = cls._GetEditor()
     if editor == ':':
@@ -80,7 +83,7 @@ least one of these before using this command.""", file=sys.stderr)
 
     fd, path = tempfile.mkstemp()
     try:
-      os.write(fd, data)
+      os.write(fd, data.encode('utf-8'))
       os.close(fd)
       fd = None
 
@@ -106,8 +109,8 @@ least one of these before using this command.""", file=sys.stderr)
         raise EditorError('editor failed with exit status %d: %s %s'
           % (rc, editor, path))
 
-      with open(path) as fd2:
-        return fd2.read()
+      with open(path, mode='rb') as fd2:
+        return fd2.read().decode('utf-8')
     finally:
       if fd:
         os.close(fd)
