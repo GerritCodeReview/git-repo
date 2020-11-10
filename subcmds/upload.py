3b77781fb7cca6c8355baced505a42c1c8463b6b
Mike Frysinger <vapier@google.com>
2020-11-10 01:09:51 -0500

    upload: improve tip for fixing upload remote
    Instead of assuming the repo client is tracking the "master" branch
    of the manifest repo, use the existing info we have to display the
    right info to the user.
    
    Bug: https://crbug.com/gerrit/13339
    Change-Id: I8b265f4b2e075fdc41909b1f3dff9aee87384353

--- subcmds/upload.py
+++ subcmds/upload.py
@@ -554,10 +554,10 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
           avail = [up_branch]
         else:
           avail = None
-          print('ERROR: Current branch (%s) not uploadable. '
-                'You may be able to type '
-                '"git branch --set-upstream-to m/master" to fix '
-                'your branch.' % str(cbr),
+          print('repo: error: Unable to upload branch "%s". '
+                'You might be able to fix the branch by running:\n'
+                '  git branch --set-upstream-to m/%s' %
+                (str(cbr), self.manifest.branch),
                 file=sys.stderr)
       else:
         avail = project.GetUploadableBranches(branch)
