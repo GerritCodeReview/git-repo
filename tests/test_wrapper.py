e634ec4df972c256b697fa542685a4b3cb2caabc
Fredrik de Groot <fredrik.de.groot@volvocars.com>
2020-12-01 15:58:53 +0100

    Fix tests after "use new main branch"
    Tests worked fine if init.defaultBranch main was used,
    but failed due to git branch reasons if master was still used.
    
    Since we can only use init.defaultBranch if git version >= 2.28,
    I also went with a template dir HEAD main tweak if lower so tests
    now pass regardless of client git default branch and version.
    
    Test: Ran tests with ~/.gitconfig:init.defaultBranch=master
    Test: Ran tests with ~/.gitconfig:init.defaultBranch=main
    Test: Ran tests for both code branches of git require
    
    Change-Id: I49fa1e4ae45b8aec16a093132ee9fa466cbc11ec

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -25,6 +25,7 @@ import shutil
 import tempfile
 import unittest
 
+import git_command
 import platform_utils
 from pyversion import is_python3
 import wrapper
@@ -357,7 +358,19 @@ class GitCheckoutTestCase(RepoWrapperTestCase):
 
     remote = os.path.join(cls.GIT_DIR, 'remote')
     os.mkdir(remote)
-    run_git('init', cwd=remote)
+
+    # Tests need to assume, that main is default branch at init,
+    # which is not supported in config until 2.28.
+    if git_command.git_require((2, 28, 0)):
+      initstr = '--initial-branch=main'
+    else:
+      # Use template dir for init.
+      templatedir = tempfile.mkdtemp(prefix='.test-template')
+      with open(os.path.join(templatedir, 'HEAD'), 'w') as fp:
+        fp.write('ref: refs/heads/main\n')
+      initstr = '--template=' + templatedir
+
+    run_git('init', initstr, cwd=remote)
     run_git('commit', '--allow-empty', '-minit', cwd=remote)
     run_git('branch', 'stable', cwd=remote)
     run_git('tag', 'v1.0', cwd=remote)
