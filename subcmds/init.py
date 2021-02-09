258033780de7b78d54f782593dd1180a8a97b506
Raman Tenneti <rtenneti@google.com>
2021-02-09 00:26:31 -0800

    init and repo: added --use-superproject option to clone superproject.
    Moved shared code between init and sync into SyncSuperproject function.
    This function either does git clone or git fetch. If git fetch fails
    it does git clone.
    
    Changed Superproject constructor to accept manifest to avoid passing
    it to multiple functions as argument.
    
    Changed functions that were raising exceptions to return either True
    or False.
    
    Saved the --use-superproject option in config as repo.superproject.
    
    Updated the tests to work with the new API changes in Superproject.
    
    Performance for the first time sync has improved from 20 minutes to
    around 17 minutes.
    
    Tested the code with the following commands.
    
    $ ./run_tests -v
    
    Tested the sync code by using repo_dev alias and pointing to this CL.
    
    $ repo init took around 20 seconds longer because of cloning of superproject.
    
    $ time repo_dev init -u sso://android.git.corp.google.com/platform/manifest -b master --partial-clone --clone-filter=blob:limit=10M --repo-rev=main --use-superproject
    ...
    real	0m35.919s
    user	0m21.947s
    sys	0m8.977s
    
    First run
    $ time repo sync --use-superproject
    ...
    real	16m41.982s
    user	100m6.916s
    sys	19m18.753s
    
    No difference in repo sync time after the first run.
    
    Bug: https://crbug.com/gerrit/13709
    Bug: https://crbug.com/gerrit/13707
    
    Change-Id: I12df92112f46e001dfbc6f12cd633c3a15cf924b

--- subcmds/init.py
+++ subcmds/init.py
@@ -25,6 +25,7 @@ from error import ManifestParseError
 from project import SyncBuffer
 from git_config import GitConfig
 from git_command import git_require, MIN_GIT_VERSION_SOFT, MIN_GIT_VERSION_HARD
+import git_superproject
 import platform_utils
 from wrapper import Wrapper
 
@@ -134,6 +135,8 @@ to update the working directory files.
     g.add_option('--submodules',
                  dest='submodules', action='store_true',
                  help='sync any submodules associated with the manifest repo')
+    g.add_option('--use-superproject', action='store_true',
+                 help='use the manifest superproject to sync projects')
     g.add_option('-g', '--groups',
                  dest='groups', default='default',
                  help='restrict manifest projects to ones with specified '
@@ -175,6 +178,20 @@ to update the working directory files.
   def _RegisteredEnvironmentOptions(self):
     return {'REPO_MANIFEST_URL': 'manifest_url',
             'REPO_MIRROR_LOCATION': 'reference'}
+
+  def _CloneSuperproject(self, branch=None):
+    """Clone the superproject based on the superproject's url and branch.
+
+    Args:
+      branch: The branchname to be passed as argument to git clone.
+
+    Returns:
+      True if superproject update is successful, otherwise False.
+    """
+    superproject = git_superproject.Superproject(self.manifest)
+    if not superproject.SyncSuperproject(branch):
+      print('error: git update of superproject failed', file=sys.stderr)
+      return False
 
   def _SyncManifest(self, opt):
     m = self.manifest.manifestProject
@@ -304,6 +321,9 @@ to update the working directory files.
 
     if opt.submodules:
       m.config.SetString('repo.submodules', 'true')
+
+    if opt.use_superproject:
+      m.config.SetString('repo.superproject', 'true')
 
     if not m.Sync_NetworkHalf(is_new=is_new, quiet=opt.quiet, verbose=opt.verbose,
                               clone_bundle=opt.clone_bundle,
@@ -518,6 +538,9 @@ to update the working directory files.
 
     self._SyncManifest(opt)
     self._LinkManifest(opt.manifest_name)
+
+    if opt.use_superproject:
+      self._CloneSuperproject(branch=opt.manifest_branch)
 
     if os.isatty(0) and os.isatty(1) and not self.manifest.IsMirror:
       if opt.config_name or self._ShouldConfigureUser(opt):
