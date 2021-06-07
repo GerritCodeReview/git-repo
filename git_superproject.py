5518172264ec2b91e3a07814ac3291442e852f4a
Raman Tenneti <rtenneti@google.com>
2021-06-07 13:27:37 -0700

    superproject: add projects from local manifest to local-no-superproject-group.
    With repo sync --use-superproject, don't update the commit ids of every project
    with the local-no-superproject-group group tag.
    
    Tested the code with the following commands.
    
    $ ./run_tests -v
    
    + Test with local.xml
    
    1. repo init --use-superproject -u persistent-https://googleplex-android.git.corp.google.com/a/platform/manifest
    
    2. cd .repo
    cp -r /google/src/head/depot/google3/wireless/android/build_tools/translations/pipeline/local_manifests local_manifests
    cd ..
    
    $ repo_dev sync -j5
    NOTICE: --use-superproject is in beta; report any issues to the address described in `repo version`
    ...
    repo sync has finished successfully.
    
    $ ls -l frameworks/support/
    total 4
    -rw-r----- 1 rtenneti primarygroup 326 Jun  7 14:11 README.md
    
    Bug: [google internal] b/189360443
    Bug: [google internal] b/189139268
    Bug: https://crbug.com/gerrit/14499
    Change-Id: Ideaf268c294e9b500b2b9726ffbd733dd8d63004

--- git_superproject.py
+++ git_superproject.py
@@ -28,6 +28,7 @@ import sys
 
 from git_command import git_require, GitCommand
 from git_refs import R_HEADS
+from manifest_xml import LOCAL_NO_SUPERPROJECT_GROUP
 
 _SUPERPROJECT_GIT_NAME = 'superproject.git'
 _SUPERPROJECT_MANIFEST_NAME = 'superproject_override.xml'
@@ -267,6 +268,9 @@ class Superproject(object):
     for project in projects:
       path = project.relpath
       if not path:
+        continue
+      # Skip the project if it comes from local manifest.
+      if project.MatchesGroups([LOCAL_NO_SUPERPROJECT_GROUP]):
         continue
       commit_id = commit_ids.get(path)
       if commit_id:
