5155a2a13886f27596d8a03b0d6ee5215881a6a8
Michael Kelly <mkelly@arista.com>
2021-07-02 09:25:48 -0700

    Allow users to ignore local manifests with 'repo manifest'
    Currently users need to look up the baseline manifest by loading the
    specific manifest file.  This exposes them to the internals of how the
    manifest is stored which may potentially be fragile (eg: It was
    switched from a symlink pointing at the file in the report to an
    actual file with an 'include' tag).
    
    Instead of doing this, we can provide an option to the 'repo manifest'
    command which will emit the baseline manifest and de-couple users from
    the internal manifest details.
    
    Change-Id: I12ee9160feaa591484ae71f404bc529be500ae4e

--- manifest_xml.py
+++ manifest_xml.py
@@ -626,6 +626,14 @@ https://gerrit.googlesource.com/git-repo/+/HEAD/docs/manifest-format.md
     return set(x.strip() for x in exclude.split(','))
 
   @property
+  def UseLocalManifests(self):
+    return self._load_local_manifests
+
+  @UseLocalManifests.setter
+  def UseLocalManifests(self, value):
+    self._load_local_manifests = value
+
+  @property
   def HasLocalManifests(self):
     return self._load_local_manifests and self.local_manifests
 
