883bf865d0401876b4c136247ab4198fa6cb5cf7
Jack Neus <jackneus@google.com>
2021-07-26 23:08:54 +0000

    repo: Add support for standalone manifests
    Added --standalone_manifest to repo tool. If set, the
    manifest is downloaded directly from the appropriate source
    (currently, we only support GS) and used instead of creating
    a manifest git checkout. The manifests.git repo is still created to
    keep track of various config but is marked as being for a standalone
    manifest so that the repo tool doesn't try to run networked git
    commands in it.
    
    BUG=b:192664812
    TEST=existing tests (no coverage), manual runs
    
    Change-Id: I84378cbc7f8e515eabeccdde9665efc8cd2a9d21

---
+++ fetch.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+import sys
+import urllib
+
+""" This module contains functions used to fetch files from various sources."""
+
+def fetch_file(url):
+    """Fetch a file from the specified source using the appropriate protocol.
+
+        Returns: (str) the contents of the file.
+    """
+    schema = urllib.parse(url).schema
+    if schema == 'gs':
+        cmd = ['gsutil', 'cat', url]
+        try:
+            result = subprocess.run(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                encoding='utf-8')
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            print('fatal: error running "gsutil": %s' % e.output,
+                  file=sys.stderr)
+            sys.exit(1)
+    raise ValueError('unsupported url %s' % url)
