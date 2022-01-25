c0ff59284b80a25ca9f0b7afa085592fd594ead9
Mike Frysinger <vapier@google.com>
2022-01-25 02:10:28 -0500

    init: make bad --repo-rev settings more clear
    If the user passes a bad --repo-rev setting in a new checkout, add a
    tip to the error message that their option is probably bad instead of
    just saying "unable to resolve".
    
    If the user has already initialized a checkout, we'd display a raw
    traceback which would confuse them.  Swallow that and also include
    the --repo-rev tip.
    
    Bug: https://crbug.com/gerrit/15610
    Change-Id: I5d72513c7b37bf9bb5d19862fcdfaf0d1f44e886

--- subcmds/init.py
+++ subcmds/init.py
@@ -520,8 +520,12 @@ to update the working directory files.
     # Handle new --repo-rev requests.
     if opt.repo_rev:
       wrapper = Wrapper()
-      remote_ref, rev = wrapper.check_repo_rev(
-          rp.gitdir, opt.repo_rev, repo_verify=opt.repo_verify, quiet=opt.quiet)
+      try:
+        remote_ref, rev = wrapper.check_repo_rev(
+            rp.gitdir, opt.repo_rev, repo_verify=opt.repo_verify, quiet=opt.quiet)
+      except wrapper.CloneFailure:
+        print('fatal: double check your --repo-rev setting.', file=sys.stderr)
+        sys.exit(1)
       branch = rp.GetBranch('default')
       branch.merge = remote_ref
       rp.work_git.reset('--hard', rev)
