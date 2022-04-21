81ccc0617516035921786ff19e310c4494df2b20
LaMont Jones <lamontjones@google.com>
2022-04-21 17:18:35 +0000

    project: Add missing imports
    Some imports were missed when moving manifestProject to project.py
    
    Bug: https://bugs.chromium.org/p/gerrit/issues/detail?id=15861
    Change-Id: Id8fffeaa3f88f344a13b5ab44e5403c7edd98f31

--- subcmds/init.py
+++ subcmds/init.py
@@ -24,8 +24,6 @@ from error import ManifestParseError
 from project import SyncBuffer
 from git_config import GitConfig
 from git_command import git_require, MIN_GIT_VERSION_SOFT, MIN_GIT_VERSION_HARD
-import fetch
-import platform_utils
 from wrapper import Wrapper
 
 
