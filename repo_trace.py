2a37a8a4a0e3d29da5766b400899f70928023c3c
Joanna Wang <jojwang@google.com>
2022-11-02 14:47:27 -0400

    Set tracing to always on and save to .repo/TRACE_FILE.
    - add `--trace_to_stderr` option so stderr will include trace outputs and any other errors that get sent to stderr
    - while TRACE_FILE will only include trace outputs
    
    piggy-backing on: https://gerrit-review.googlesource.com/c/git-repo/+/349154
    
    Change-Id: I3895a84de4b2784f17fac4325521cd5e72e645e2

--- repo_trace.py
+++ repo_trace.py
@@ -19,15 +19,37 @@ Activated via `repo --trace ...` or `REPO_TRACE=1 repo ...`.
 
 import sys
 import os
+import time
+from contextlib import ContextDecorator
 
 # Env var to implicitly turn on tracing.
 REPO_TRACE = 'REPO_TRACE'
 
-_TRACE = os.environ.get(REPO_TRACE) == '1'
+# Temporarily set tracing to always on.
+_TRACE = os.environ.get(REPO_TRACE) == '1' or True
+
+_TRACE_TO_STDERR = False
+
+_TRACE_FILE = None
+
+_TRACE_FILE_NAME = 'TRACE_FILE'
+
+_MAX_SIZE = 1  # in mb
+
+_NEW_COMMAND_SEP = '+++++++++++++++NEW COMMAND+++++++++++++++++++'
+
+
+def IsStraceToStderr():
+  return _TRACE_TO_STDERR
 
 
 def IsTrace():
   return _TRACE
+
+
+def SetTraceToStderr():
+  global _TRACE_TO_STDERR
+  _TRACE_TO_STDERR = True
 
 
 def SetTrace():
@@ -35,6 +57,92 @@ def SetTrace():
   _TRACE = True
 
 
-def Trace(fmt, *args):
-  if IsTrace():
-    print(fmt % args, file=sys.stderr)
+def _SetTraceFile():
+  global _TRACE_FILE
+  _TRACE_FILE = _GetTraceFile()
+
+
+class Trace(ContextDecorator):
+
+    def _time(self):
+      """Generate nanoseconds of time in a py3.6 safe way"""
+      return int(time.time()*1e+9)
+
+    def __init__(self, fmt, *args, **kwargs):
+      if not IsTrace():
+        return
+      self._trace_msg = fmt % args
+
+      if not _TRACE_FILE:
+        _SetTraceFile()
+
+      if kwargs.get('firstTrace') == 'true':
+
+        _ClearOldTraces()
+        self._trace_msg = '%s %s' % (_NEW_COMMAND_SEP, self._trace_msg)
+
+
+    def __enter__(self):
+      if not IsTrace():
+        return self
+
+      print_msg = f"PID: {os.getpid()} START: {self._time()} :" + self._trace_msg + '\n'
+
+      with open(_TRACE_FILE, 'a') as f:
+        print(print_msg, file=f)
+
+      if _TRACE_TO_STDERR:
+        print(print_msg, file=sys.stderr)
+
+      return self
+
+    def __exit__(self, *exc):
+      if not IsTrace():
+        return False
+
+      print_msg = f"PID: {os.getpid()} END: {self._time()} :" + self._trace_msg + '\n'
+
+      with open(_TRACE_FILE, 'a') as f:
+        print(print_msg, file=f)
+
+      if _TRACE_TO_STDERR:
+        print(print_msg, file=sys.stderr)
+
+      return False
+
+
+def _GetTraceFile():
+  """Get the trace file or create one."""
+  curdir = os.getcwd()
+  repodir = '.repo'
+  repo = None
+
+  olddir = None
+  while curdir != olddir and not repo:
+    repo = os.path.join(curdir, repodir, 'repo/main.py')
+    if not os.path.isfile(repo):
+      repo = None
+      olddir = curdir
+      curdir = os.path.dirname(curdir)
+  trace_file = os.path.join(curdir, repodir, _TRACE_FILE_NAME)
+  print('chicken')
+  print(trace_file)
+  return trace_file
+
+def _ClearOldTraces():
+  """Clear traces from old commands if trace file is too big.
+
+  Note: this may clear more than necessary if two repo commands
+        start at the same time.
+  """
+  if os.path.isfile(_TRACE_FILE):
+    while os.path.getsize(_TRACE_FILE)/(1024*1024) > _MAX_SIZE:
+      temp_file = _TRACE_FILE + 'tmp'
+      with open(_TRACE_FILE, 'r', errors='ignore') as fin:
+        with open(temp_file, 'w') as tf:
+          trace_lines = fin.readlines()
+          for i , l in enumerate(trace_lines):
+            if 'END:' in l and _NEW_COMMAND_SEP in l:
+              tf.writelines(trace_lines[i+1:])
+              break
+      os.replace(temp_file, _TRACE_FILE)
