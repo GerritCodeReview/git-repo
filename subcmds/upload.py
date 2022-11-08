fa8d1d8e8ef84ad8f0dc48d2fc99f88a6400bbe1
LaMont Jones <lamontjones@google.com>
2022-11-08 23:10:27 +0000

    upload: track projects by path, rather than name
    Since the same project can be checked out in multiple paths, we need to
    track the "to be uploaded" projects by path, rather than project name.
    
    Bug: crbug.com/gerrit/16260
    Test: manual
    Change-Id: Ic3dc81bb8acb34886baa6299e90a49c7ba372957

--- subcmds/upload.py
+++ subcmds/upload.py
@@ -302,8 +302,9 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
           script.append('#         %s' % commit)
         b[name] = branch
 
-      projects[project.RelPath(local=opt.this_manifest_only)] = project
-      branches[project.name] = b
+      project_path = project.RelPath(local=opt.this_manifest_only)
+      projects[project_path] = project
+      branches[project_path] = b
     script.append('')
 
     script = Editor.EditString("\n".join(script)).split("\n")
@@ -328,9 +329,10 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
         name = m.group(1)
         if not project:
           _die('project for branch %s not in script', name)
-        branch = branches[project.name].get(name)
+        project_path = project.RelPath(local=opt.this_manifest_only)
+        branch = branches[project_path].get(name)
         if not branch:
-          _die('branch %s not in %s', name, project.RelPath(local=opt.this_manifest_only))
+          _die('branch %s not in %s', name, project_path)
         todo.append(branch)
     if not todo:
       _die("nothing uncommented for upload")
