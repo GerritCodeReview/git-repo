51b6fb8c40b03ce32df1211e6793005033689e62
Daniel Kutik <daniel.kutik@lavawerk.com>
2022-11-27 13:26:15 +0100

    test: fix path seperator errors on windows
    Fixing multiple errors when running tests on Windows related
    to path seperator being different ('\' instead of '/').
    
    Signed-off-by: Daniel Kutik <daniel.kutik@lavawerk.com>
    Change-Id: I26b44d092b925edecab46a4d88e77dd9dcb8df28

--- tests/test_project.py
+++ tests/test_project.py
@@ -384,7 +384,7 @@ class MigrateWorkTreeTests(unittest.TestCase):
 
       # Make sure the dir was transformed into a symlink.
       self.assertTrue(dotgit.is_symlink())
-      self.assertEqual(os.readlink(dotgit), '../../.repo/projects/src/test.git')
+      self.assertEqual(os.readlink(dotgit), os.path.normpath('../../.repo/projects/src/test.git'))
 
       # Make sure files were moved over.
       gitdir = tempdir / '.repo/projects/src/test.git'
