89849860898b6358d3f79ce0de0c32920ef85624
LaMont Jones <lamontjones@google.com>
2022-12-02 22:54:11 +0000

    sync: finish marking REPO_AUTO_GC=1 as deprecated.
    The wrong revision of the change was submitted as
    d793553804c76677444709ebefd70f6e01c29525.
    
    Change-Id: I6f3e4993cf40c30ccf0d69020177db8fe5f76b8c

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -67,8 +67,7 @@ from manifest_xml import GitcManifest
 _ONE_DAY_S = 24 * 60 * 60
 
 # Env var to implicitly turn auto-gc back on.  This was added to allow a user to
-# revert a change in default behavior in v2.29.9, and will be removed in a
-# future release.
+# revert a change in default behavior in v2.29.9.  Remove after 2023-04-01.
 _REPO_AUTO_GC = 'REPO_AUTO_GC'
 _AUTO_GC = os.environ.get(_REPO_AUTO_GC) == '1'
 
@@ -1186,10 +1185,9 @@ later is required to fix a server side protocol bug.
 
     if opt.auto_gc is None and _AUTO_GC:
       print(f"Will run `git gc --auto` because {_REPO_AUTO_GC} is set.",
-            file=sys.stderr)
-      opt.auto_gc = True
-      print(f'{_REPO_AUTO_GC} is deprecated and will be removed in a future'
+            f'{_REPO_AUTO_GC} is deprecated and will be removed in a future',
             'release.  Use `--auto-gc` instead.', file=sys.stderr)
+      opt.auto_gc = True
 
   def Execute(self, opt, args):
     manifest = self.outer_manifest
