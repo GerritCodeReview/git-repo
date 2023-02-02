3863bc8dfd04801d4b6bc8ca3b7ea0a3aa79f399
Tyler Freeman <fuego@google.com>
2023-02-01 16:05:02 -0800

    feat(autosquash): add autosquash feature to repo upload
    Autosquash squashes all the commits in your current branch
    so you can treat an entire brach as one CL instead of the standard
    one CL per commit.
    
    Also add a standalone command 'repo autosquash .'
    
    Test: repo upload --cbr --autosquash .
    Change-Id: I06045c115ec58b05df95cd62d2e8937339d6698d

--- subcmds/upload.py
+++ subcmds/upload.py
@@ -26,6 +26,7 @@ from git_command import GitCommand
 from git_refs import R_HEADS
 from hooks import RepoHook
 from project import ReviewableBranch
+from subcmds.autosquash import Autosquasher
 
 
 _DEFAULT_UNUSUAL_COMMIT_THRESHOLD = 5
@@ -264,6 +265,9 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
     p.add_option('--no-cert-checks',
                  dest='validate_certs', action='store_false', default=True,
                  help='disable verifying ssl certs (unsafe)')
+    p.add_option('--autosquash',
+                 dest='autosquash', action='store_true',
+                 help='squash commits first so each branch is its own CL')
     RepoHook.AddOptionGroup(p, 'pre-upload')
 
   def _SingleBranch(self, opt, branch, people):
@@ -575,6 +579,24 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
   def Execute(self, opt, args):
     projects = self.GetProjects(args, all_manifests=not opt.this_manifest_only)
 
+    autosquasher = None
+    try:
+      if (opt.autosquash):
+        if (len(projects) == 1):
+          project = projects[0]
+          autosquasher = Autosquasher(project, project.CurrentBranch)
+          autosquasher.Squash()
+        else:
+          print('WARNING: Autosquash aborted. Autosquash doesn\'t work on multiple projects. Did you make sure to specify only the current directory?')
+          return 1
+
+      self.ExecuteUpload(opt, args, projects)
+    finally:
+      if autosquasher is not None:
+        autosquasher.Restore()
+
+
+  def ExecuteUpload(self, opt, args, projects):
     def _ProcessResults(_pool, _out, results):
       pending = []
       for result in results:
