2e8e75a1211978e9d82b162517b813aa7da935bc
Jason R. Coombs <jaraco@jaraco.com>
2023-09-29 11:04:49 -0400

    cleanup: Update codebase to expect Python 3.6
    - Bump minimum version to Python 3.6.
    - Ran pyupgrade for Python 3.6, then again, then
      - ran black
      - fixed a lingering unused import, and
      - removed a duplicate exception.
    - Replaced use of .format with f-strings or .format_map.
    - Manually converted some percent formatting and removed a redundant try/except.
    
    Change-Id: I2aa70197230fcec2eff8e7c8eb754f20c08075bb

--- main.py
+++ main.py
@@ -198,9 +198,8 @@ class _Repo:
         if short:
             commands = " ".join(sorted(self.commands))
             wrapped_commands = textwrap.wrap(commands, width=77)
-            print(
-                "Available commands:\n  %s" % ("\n  ".join(wrapped_commands),)
-            )
+            sep = "\n  "
+            print(f"Available commands:\n  {sep.join(wrapped_commands)}")
             print("\nRun `repo help <command>` for command-specific details.")
             print("Bug reports:", Wrapper().BUG_URL)
         else:
@@ -236,7 +235,7 @@ class _Repo:
         if name in self.commands:
             return name, []
 
-        key = "alias.%s" % (name,)
+        key = f"alias.{name}"
         alias = RepoConfig.ForRepository(self.repodir).GetString(key)
         if alias is None:
             alias = RepoConfig.ForUser().GetString(key)
