987db0095ba62af1ac1af76025b74e1b4b105c35
Masaki Muranaka <monaka@monami-ya.com>
2023-09-30 11:04:39 +0900

    The return value of getting `repo.depth` should be `int`. Not  `str`.
    Bug: https://issues.gerritcodereview.com/40015442
    Change-Id: I8fed1f020fbae06c20a64b505813851debde9504
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- project.py
+++ project.py
@@ -3982,7 +3982,7 @@ class ManifestProject(MetaProject):
     @property
     def depth(self):
         """Partial clone depth."""
-        return self.config.GetString("repo.depth")
+        return self.config.GetInt("repo.depth")
 
     @property
     def clone_filter(self):
