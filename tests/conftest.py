c8ce189904c0016f9f35d50508dcd0746b241023
Jason R. Coombs <jaraco@google.com>
2023-10-02 13:58:54 -0400

    tests: Set HOME to a temporary directory when running tests.
    Bug: 302797407
    
    Change-Id: Ib42b712dd7b6602fee6e18329a8c6d52fb9458b9

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,11 @@
 
 """Common fixtures for pytests."""
 
+import shutil
+
 import pytest
 
+import platform_utils
 import repo_trace
 
 
@@ -23,3 +26,36 @@ import repo_trace
 def disable_repo_trace(tmp_path):
     """Set an environment marker to relax certain strict checks for test code."""  # noqa: E501
     repo_trace._TRACE_FILE = str(tmp_path / "TRACE_FILE_from_test")
+
+
+# copied from
+# https://github.com/pytest-dev/pytest/issues/363#issuecomment-1335631998
+@pytest.fixture(scope="session")
+def monkeysession():
+    with pytest.MonkeyPatch.context() as mp:
+        yield mp
+
+
+@pytest.fixture(autouse=True, scope="session")
+def alt_home(tmp_path_factory, monkeysession):
+    """Set HOME to a temporary directory, avoiding user's .gitconfig.
+
+    b/302797407
+
+    Modeled after PyPI:pytest-home, but with session scope. Session
+    scope is necessary to take effect prior to
+    ``test_wrapper.GitCheckoutTestCase.setUpClass``.
+    """
+    vars = ["HOME"] + platform_utils.isWindows() * ["USERPROFILE"]
+    home = tmp_path_factory.mktemp("home")
+    for var in vars:
+        monkeysession.setenv(var, str(home))
+    return home
+
+
+@pytest.fixture(autouse=True)
+def clean_home(alt_home):
+    """Ensure HOME is cleaned after each test."""
+    yield alt_home
+    shutil.rmtree(alt_home)
+    alt_home.mkdir()
