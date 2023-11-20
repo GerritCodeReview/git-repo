52e5b8fa3a6428dc0fced6c6e8e2410761969602
Josip Sokcevic <sokcevic@google.com>
2023-11-20 10:31:53 -0800

    info: Handle undefined mergeBranch
    Bug: b/308025460
    Change-Id: Iebceac0976e5d3adab5300bd8dfc76744a791234

--- subcmds/info.py
+++ subcmds/info.py
@@ -97,7 +97,7 @@ class Info(PagedCommand):
             self.headtext(self.manifest.default.revisionExpr)
         self.out.nl()
         self.heading("Manifest merge branch: ")
-        self.headtext(mergeBranch)
+        self.headtext(mergeBranch or "")
         self.out.nl()
         self.heading("Manifest groups: ")
         self.headtext(manifestGroups)
