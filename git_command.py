818803536f620e353ff648050b6ca4db7a4f6cbe
Mike Frysinger <vapier@google.com>
2024-03-21 12:58:01 -0400

    git_command: unify soft/hard versions with requirements.json
    Use the requirements logic in the wrapper to load versions out of the
    requirements.json file to avoid duplicating them in git_command.py.
    
    Change-Id: Ib479049fc54ebc6f52c2c30d1315cf1734ff1990

--- git_command.py
+++ git_command.py
@@ -33,17 +33,6 @@ from wrapper import Wrapper
 
 
 GIT = "git"
-# NB: These do not need to be kept in sync with the repo launcher script.
-# These may be much newer as it allows the repo launcher to roll between
-# different repo releases while source versions might require a newer git.
-#
-# The soft version is when we start warning users that the version is old and
-# we'll be dropping support for it.  We'll refuse to work with versions older
-# than the hard version.
-#
-# git-1.7 is in (EOL) Ubuntu Precise.  git-1.9 is in Ubuntu Trusty.
-MIN_GIT_VERSION_SOFT = (1, 9, 1)
-MIN_GIT_VERSION_HARD = (1, 7, 2)
 GIT_DIR = "GIT_DIR"
 
 LAST_GITDIR = None
