d893f38edf819a36d9fa2d313809ec21bb583880
Xinyu Xu <xu@arista.com>
2024-07-24 18:03:59 +0000

    sync: Add --only-manifest-update option
    In some cases we may only want to update the manifest files but not the
    packages specified in that manifest file. This commit adds a feature to
    skip fetching and updating the packages. This can potentially reduce the
    load on gerrit server.
    
    Change-Id: I635985d2b1b02ece0b63ae282b6cbe302887f514

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -417,6 +417,14 @@ later is required to fix a server side protocol bug.
             "(do not update to the latest revision)",
         )
         p.add_option(
+            "--only-manifest-update",
+            "--omu",
+            dest="omp_update",
+            action="store_true",
+            help="only update the manifest file, skipping fetch and checkout. "
+            "(do not update the projects)",
+        )
+        p.add_option(
             "-n",
             "--network-only",
             dest="network_only",
@@ -1757,6 +1765,10 @@ later is required to fix a server side protocol bug.
             self._UpdateAllManifestProjects(opt, mp, manifest_name, errors)
         else:
             print("Skipping update of local manifest project.")
+
+        if opt.omp_update:
+            print("Skipping update projects.")
+            return
 
         # Now that the manifests are up-to-date, setup options whose defaults
         # might be in the manifest.
