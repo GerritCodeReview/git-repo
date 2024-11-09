ec1f0a6376d2ae81f9da77bf7652dba12438e8be
SyaifulNizam Shamsudin <syaifulnizamshamsudin@gmail.com>
2024-11-09 10:32:43 +0000

    Edit setup.py
    Change-Id: Ida6707204b966535ad4a063fc2195bb3b7136599
    Signed-off-by: SyaifulNizam Shamsudin <syaifulnizamshamsudin@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# Copyright 2019 The Android Open Source Project
-#
+# Copyright©️2019 The Android Open Source Project
+# Android Developer:syaifulnizamshamsudin@gmail.com
+# <status@stspg.io>
 # Licensed under the Apache License, Version 2.0 (the 'License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
