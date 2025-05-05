065a2ce225708346b2314a18285a682f37b57d98
Gavin Mak <gavinmak@google.com>
2025-05-05 14:13:48 -0700

    upload: Add superproject identifier as push option
    When uploading, add the root superproject repo as a push option in the
    format `-o custom-key-value=rootRepo:$HOST/$PROJECT`.
    
    Bug: b/401147338
    Change-Id: I00230256eb7ae307b03840bb4090c28dc8a0505e

--- subcmds/upload.py
+++ subcmds/upload.py
@@ -628,6 +628,13 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
                 branch.uploaded = False
                 return
 
+        # If using superproject, add the root repo as a push option.
+        manifest = branch.project.manifest
+        push_options = list(opt.push_options)
+        sp = manifest.superproject
+        if sp and sp.repo_id and manifest.manifestProject.use_superproject:
+            push_options.append(f"custom-keyed-value=rootRepo:{sp.repo_id}")
+
         branch.UploadForReview(
             people,
             dryrun=opt.dryrun,
@@ -640,7 +647,7 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
             ready=opt.ready,
             dest_branch=destination,
             validate_certs=opt.validate_certs,
-            push_options=opt.push_options,
+            push_options=push_options,
             patchset_description=opt.patchset_description,
         )
 
