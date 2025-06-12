2fc5ccb22cc2878336787670c894c8cf4c58f977
Gavin Mak <gavinmak@google.com>
2025-06-11 23:37:32 -0700

    sync: worker
    Change-Id: I398602e08a375e974a8914e5fa48ffae673dda9b

--- subcmds/sync.py
+++ subcmds/sync.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import collections
+import contextlib
 import functools
 import http.cookiejar as cookielib
 import io
@@ -198,8 +199,10 @@ class _SyncResult(NamedTuple):
     """Individual project sync result for interleaved mode.
 
     Attributes:
+      project_index: The index of the project in the shared list.
       relpath: The relative path of the project from the top of the repo client.
       fetch_success: True if the fetch operation was successful.
+      remote_fetched: True if the remote was actually queried.
       checkout_success: True if the checkout operation was successful.
       fetch_error: The Exception object if the fetch failed, else None.
       checkout_error: The Exception object if the checkout failed, else None.
@@ -210,12 +213,13 @@ class _SyncResult(NamedTuple):
           finished.
     """
 
+    project_index: int
     relpath: str
     fetch_success: bool
+    remote_fetched: bool
     checkout_success: bool
     fetch_error: Optional[Exception]
     checkout_error: Optional[Exception]
-
     fetch_start: Optional[float]
     fetch_finish: Optional[float]
     checkout_start: Optional[float]
@@ -2065,31 +2069,112 @@ later is required to fix a server side protocol bug.
         context = cls.get_parallel_context()
         projects = context["projects"]
         sync_dict = context["sync_dict"]
+        ssh_proxy = context.get("ssh_proxy")
 
         # Use the first project as the representative for the progress bar.
         first_project = projects[project_indices[0]]
         key = f"{first_project.name} @ {first_project.relpath}"
-        start_time = time.time()
-        sync_dict[key] = start_time
+        sync_dict[key] = time.time()
 
         try:
             for idx in project_indices:
                 project = projects[idx]
-                # For now, simulate a successful sync.
-                # TODO(b/421935613): Perform the actual git fetch and checkout.
-                results.append(
-                    _SyncResult(
-                        relpath=project.relpath,
-                        fetch_success=True,
-                        checkout_success=True,
-                        fetch_error=None,
-                        checkout_error=None,
-                        fetch_start=None,
-                        fetch_finish=None,
-                        checkout_start=None,
-                        checkout_finish=None,
+
+                fetch_success = False
+                checkout_success = False
+                fetch_error = None
+                checkout_error = None
+                fetch_start = None
+                fetch_finish = None
+                checkout_start = None
+                checkout_finish = None
+                remote_fetched = False
+
+                # Fetch phase.
+                if not opt.local_only:
+                    fetch_start = time.time()
+                    try:
+                        sync_result = project.Sync_NetworkHalf(
+                            quiet=opt.quiet,
+                            verbose=opt.verbose,
+                            output_redir=io.StringIO(),
+                            current_branch_only=cls._GetCurrentBranchOnly(
+                                opt, project.manifest
+                            ),
+                            force_sync=opt.force_sync,
+                            clone_bundle=opt.clone_bundle,
+                            tags=opt.tags,
+                            archive=project.manifest.IsArchive,
+                            optimized_fetch=opt.optimized_fetch,
+                            retry_fetches=opt.retry_fetches,
+                            prune=opt.prune,
+                            ssh_proxy=ssh_proxy,
+                            clone_filter=project.manifest.CloneFilter,
+                            partial_clone_exclude=project.manifest.PartialCloneExclude,
+                            clone_filter_for_depth=project.manifest.CloneFilterForDepth,
+                        )
+                        fetch_success = sync_result.success
+                        remote_fetched = sync_result.remote_fetched
+                        if sync_result.error:
+                            fetch_error = sync_result.error
+                    except Exception as e:
+                        fetch_success = False
+                        fetch_error = e
+                    finally:
+                        fetch_finish = time.time()
+                else:
+                    fetch_success = True
+
+                # Checkout phase.
+                if not fetch_success:
+                    checkout_success = False
+                elif opt.network_only:
+                    checkout_success = True
+                else:
+                    checkout_start = time.time()
+                    stderr_capture = io.StringIO()
+                    with contextlib.redirect_stderr(stderr_capture):
+                        syncbuf = SyncBuffer(
+                            project.manifest.manifestProject.config,
+                            detach_head=opt.detach_head,
+                        )
+                        local_half_errors = []
+                        try:
+                            project.Sync_LocalHalf(
+                                syncbuf,
+                                force_sync=opt.force_sync,
+                                force_checkout=opt.force_checkout,
+                                force_rebase=opt.rebase,
+                                errors=local_half_errors,
+                                verbose=opt.verbose,
+                            )
+                            checkout_success = syncbuf.Finish()
+                            if local_half_errors:
+                                checkout_error = SyncError(
+                                    aggregate_errors=local_half_errors
+                                )
+                        except Exception as e:
+                            checkout_success = False
+                            checkout_error = e
+                        finally:
+                            checkout_finish = time.time()
+                    stderr_text = stderr_capture.getvalue()
+                    results.append(
+                        _SyncResult(
+                            project_index=idx,
+                            relpath=project.relpath,
+                            fetch_success=fetch_success,
+                            checkout_success=checkout_success,
+                            fetch_error=fetch_error,
+                            checkout_error=checkout_error,
+                            fetch_start=fetch_start,
+                            fetch_finish=fetch_finish,
+                            checkout_start=checkout_start,
+                            checkout_finish=checkout_finish,
+                            remote_fetched=remote_fetched,
+                            stderr_text=stderr_text,
+                        )
                     )
-                )
         finally:
             del sync_dict[key]
 
@@ -2107,9 +2192,41 @@ later is required to fix a server side protocol bug.
     ):
         """Callback to process results from interleaved sync workers."""
         ret = True
+        projects = self.get_parallel_context()["projects"]
         for result_group in results_sets:
             for result in result_group.results:
                 pm.update()
+                project = projects[result.project_index]
+
+                if opt.verbose and result.stderr_text:
+                    # Erase the progress bar line before printing messages.
+                    pm.out.write("\r" + pm.CSI_ERASE_LINE)
+                    pm.out.write(result.stderr_text)
+
+                if result.fetch_start:
+                    self._fetch_times.Set(
+                        project,
+                        result.fetch_finish - result.fetch_start,
+                    )
+                    self._local_sync_state.SetFetchTime(project)
+                    self.event_log.AddSync(
+                        project,
+                        event_log.TASK_SYNC_NETWORK,
+                        result.fetch_start,
+                        result.fetch_finish,
+                        result.fetch_success,
+                    )
+                if result.checkout_start:
+                    if result.checkout_success:
+                        self._local_sync_state.SetCheckoutTime(project)
+                    self.event_log.AddSync(
+                        project,
+                        event_log.TASK_SYNC_LOCAL,
+                        result.checkout_start,
+                        result.checkout_finish,
+                        result.checkout_success,
+                    )
+
                 if result.fetch_success and result.checkout_success:
                     synced_relpaths.add(result.relpath)
                 else:
