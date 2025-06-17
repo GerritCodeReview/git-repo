47aeff9c1763a25b2665a19ba97064abb8cb3500
Gavin Mak <gavinmak@google.com>
2025-06-17 12:04:26 -0700

    upload: fix FileNotFoundError when no superproject
    Upload gets a FileNotFoundError if not using superproject because it
    tries to access the superproject's repo_id before checking if
    superproject was actually enabled.
    
    Reorders the logic to check the use_superproject first.
    
    Change-Id: I65cd2adab481e799dd7bb75e1a83553ad6e34d8d

--- subcmds/upload.py
+++ subcmds/upload.py
@@ -627,9 +627,12 @@ Gerrit Code Review:  https://www.gerritcodereview.com/
         # If using superproject, add the root repo as a push option.
         manifest = branch.project.manifest
         push_options = list(opt.push_options)
-        sp = manifest.superproject
-        if sp and sp.repo_id and manifest.manifestProject.use_superproject:
-            push_options.append(f"custom-keyed-value=rootRepo:{sp.repo_id}")
+        if manifest.manifestProject.use_superproject:
+            sp = manifest.superproject
+            if sp:
+                repo_id = sp.repo_id
+                if repo_id:
+                    push_options.append(f"custom-keyed-value=rootRepo:{repo_id}")
 
         branch.UploadForReview(
             people,
