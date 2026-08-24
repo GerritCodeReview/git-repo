b4443962f01fb42ad63d114de9a3c0fcb4a36d5d
kimhappy <hwanhee.kim@laplacian.cc>
2026-08-24 11:09:10 +0900

    project: allow re-reading submodule gitlinks
    A discovered submodule becomes a project whose revision is the gitlink
    read from its parent at manifest load time, which is before the parent
    has been fetched. Remember which gitlink a submodule was derived from,
    and add a way to read those gitlinks again, so that callers can resolve
    a submodule revision once its parent is up-to-date.
    
    Bug: 550074864
    Change-Id: I4f35d53607126508eec6f02158a3fb604917a199
    Signed-off-by: kimhappy <hwanhee.kim@laplacian.cc>

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python3
+            pkgs.python3Packages.flake8
+          ];
+        };
+      }
+    );
+}
